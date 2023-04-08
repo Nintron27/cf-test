344711161c8c328dfbc1898487fe07040b2cace5
Nintron <nintron@sent.com>
2023-04-08 16:56:01 -0400

    SSR fast function

---
+++ src/routes/random/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	export const csr = false;
+
+	let func = () => {
+		console.log('HELLO');
+		return Math.random();
+	};
+	const random = func();
+</script>
+
+<h1>Here's your random: {random}</h1>
