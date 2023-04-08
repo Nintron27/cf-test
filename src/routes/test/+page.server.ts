f9553db455dc961f2b1c037bda86f579b92cc91d
Nintron <nintron@sent.com>
2023-04-08 18:47:46 -0400

    Give up on polyfill for now

--- src/routes/test/+page.server.ts
+++ src/routes/test/+page.server.ts
@@ -1,35 +1,22 @@
 import type { PageServerLoad } from "./$types";
 
-import { installPolyfills } from "@sveltejs/kit/node/polyfills";
 
 export const load = (async () => {
-    installPolyfills();
-    // if (performance) {
-    //     let requestStartTime = new Date().getTime();
+    let requestStartTime = new Date().getTime();
 
-    //     let response = await fetch("https://catfact.ninja/fact");
-        
-    //     let requestTime =  new Date().getTime() - requestStartTime;
+    let response = await fetch("https://catfact.ninja/fact");
     
-    //     let deserializeStartTime = new Date().getTime();
-    
-    //     let json = await response.json();
-    
-    //     let deserializeTime =  new Date().getTime() - deserializeStartTime;
-       
-    //     return { 
-    //         "json": json, 
-    //         "requestTime": requestTime, 
-    //         "deserializeTime": deserializeTime 
-    //     };
-    // } else {
-    //     return {
-    //         "status": "NOPE"
-    //     }
-    // }
+    let requestTime =  new Date().getTime() - requestStartTime;
 
-    return {
-        "ohYEAH": crypto.randomUUID()
-    }
+    let deserializeStartTime = new Date().getTime();
 
+    let json = await response.json();
+
+    let deserializeTime =  new Date().getTime() - deserializeStartTime;
+    
+    return { 
+        "json": json, 
+        "requestTime": requestTime, 
+        "deserializeTime": deserializeTime 
+    };
 }) satisfies PageServerLoad;
--- src/routes/test/+page.svelte
+++ src/routes/test/+page.svelte
@@ -5,11 +5,6 @@
 </script>
 
 <h1>Hello big test</h1>
-<p>oh papi: {data.ohYEAH}</p>
-<!-- {#if data.json}
-	<p>deserialize time: {data.deserializeTime}</p>
-	<p>request time: {data.requestTime}</p>
-	<p>json: {data.json}</p>
-{:else if data.ohYEAH }
-	<p>Thats a big nope papi</p>
-{/if} -->
+<p>deserialize time: {data.deserializeTime}</p>
+<p>request time: {data.requestTime}</p>
+<p>json: {data.json}</p>
